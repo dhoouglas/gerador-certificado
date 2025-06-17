3a2acabc452bc80cd8552f998f7139be7b3ef7b4
dhoouglas <dhoouglas@hotmail.com>
2025-06-17 11:31:15 -0300

    feat: validating form fields

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButtonComponent, PrimaryButtonComponent],
+  imports: [
+    SecondaryButtonComponent,
+    PrimaryButtonComponent,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './certificado-form.component.html',
   styleUrl: './certificado-form.component.css',
 })
-export class CertificadoFormComponent {}
+export class CertificadoFormComponent {
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['Angular', 'React', 'Cobol'];
+
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched;
+  }
+}
